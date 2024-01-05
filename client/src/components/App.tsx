b7fc6aa9b03eec8b55f0c7b14732b0dc556fcfcb
Matt Hackmann <mhackmann13@icloud.com>
2024-01-05 11:38:20 -0600

    style CustomerItem

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,63 +1,48 @@
-const randoms = [
-  [1, 2],
-  [3, 4, 5],
-  [6, 7]
-];
+import CustomerItem from './CustomerItem';
 
 function App() {
   return (
     <div className="relative overflow-hidden bg-white">
-      <div className="h-screen sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
-        <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
-          <div className="sm:max-w-lg">
-            <div className="my-4"></div>
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Welcome!
-            </h1>
-            <p className="mt-4 text-xl text-gray-500">
-              This is a boilerplate build with Vite, React 18, TypeScript,
-              Vitest, Testing Library, TailwindCSS 3, Eslint and Prettier.
-            </p>
+      <div className="m-4 w-1/2">
+        <div className="mb-1 flex justify-between text-sm font-semibold">
+          <div>
+            <span className="inline-block w-20 pl-2">Status</span>
+            <span className="inline-block pl-2">Customer Name</span>
           </div>
           <div>
-            <div className="my-10">
-              <a
-                href="vscode://"
-                className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-offset-2"
-              >
-                Start building for free
-              </a>
-              <div
-                aria-hidden="true"
-                className="pointer-events-none mt-10 md:mt-0 lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
-              >
-                <div className="absolute sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
-                  <div className="flex items-center space-x-6 lg:space-x-8">
-                    {randoms.map((random, number) => (
-                      <div
-                        key={`random-${random[number]}`}
-                        className="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8"
-                      >
-                        {random.map((number) => (
-                          <div
-                            key={`random-${number}`}
-                            className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100"
-                          >
-                            <img
-                              src={`https://picsum.photos/600?random=${number}`}
-                              alt=""
-                              className="h-full w-full bg-indigo-100 object-cover object-center"
-                            />
-                          </div>
-                        ))}
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </div>
+            <span className="inline-block w-32 pl-1">Check In Time</span>
+            <span className="inline-block w-24 pl-1">Time Called</span>
           </div>
         </div>
+        <ul>
+          <li className="mb-1">
+            <CustomerItem
+              status="Serving"
+              name="John Doe"
+              checkInTime={new Date()}
+              callTime={new Date()}
+              onClick={() => console.log('dsf')}
+            />
+          </li>
+          <li className="mb-1">
+            <CustomerItem
+              status="Served"
+              name="John Doe"
+              checkInTime={new Date()}
+              callTime={new Date()}
+              onClick={() => console.log('dsf')}
+            />
+          </li>
+          <li className="mb-1">
+            <CustomerItem
+              status="Waiting"
+              name="John Doe"
+              checkInTime={new Date()}
+              callTime={new Date()}
+              onClick={() => console.log('dsf')}
+            />
+          </li>
+        </ul>
       </div>
     </div>
   );
--- client/src/components/CustomerItem.tsx
+++ client/src/components/CustomerItem.tsx
@@ -1,31 +1,68 @@
 import { ReactEventHandler } from 'react';
 
+type CustomerStatus = 'Waiting' | 'Serving' | 'Served' | 'No Show';
+
 interface CustomerItemComponent {
   status: CustomerStatus;
   name: string;
   checkInTime: Date;
   callTime: Date;
+  selected?: boolean;
   onClick: ReactEventHandler;
 }
-
-type CustomerStatus = 'Waiting' | 'Serving' | 'Served' | 'No Show';
 
 export default function CustomerItem({
   status,
   name,
   checkInTime,
   callTime,
+  selected = false,
   onClick
 }: CustomerItemComponent) {
+  const containerStylesByStatus = {
+    Waiting: 'border-slate_gray outline-slate_gray',
+    Serving: 'border-green-500 outline-green-500',
+    Served: 'border-french_gray_2 outline-french_gray_2',
+    'No Show': 'border-french_gray_2 outline-french_gray_2'
+  };
+
+  const statusTextColors = {
+    Waiting: 'text-slate_gray',
+    Serving: 'text-green-500',
+    Served: 'text-french_gray_2',
+    'No Show': 'text-french_gray_2'
+  };
+
+  const get12HourTimeString = (date: Date) => {
+    const minutes = date.getMinutes();
+
+    return `${date.getHours() % 12}:${minutes < 10 ? `0${minutes}` : minutes} ${
+      minutes > 12 ? 'PM' : 'AM'
+    }`;
+  };
+
   return (
-    <div onClick={onClick}>
+    <div
+      onClick={onClick}
+      className={`hover:bg-seasalt flex justify-between rounded-lg border-2 p-2 hover:cursor-pointer ${
+        containerStylesByStatus[status]
+      } ${selected ? 'bg-antiflash_white-500 outline' : 'bg-transparent'}`}
+    >
       <div>
-        <span>{status}</span>
+        <span
+          className={`inline-block w-20 font-medium ${statusTextColors[status]}`}
+        >
+          {status}
+        </span>
         <span>{name}</span>
       </div>
       <div>
-        <span>{checkInTime.toUTCString()}</span>
-        <span>{callTime.toUTCString()}</span>
+        <span className="inline-block w-32">
+          {get12HourTimeString(checkInTime)}
+        </span>
+        <span className="inline-block w-20">
+          {get12HourTimeString(callTime)}
+        </span>
       </div>
     </div>
   );
