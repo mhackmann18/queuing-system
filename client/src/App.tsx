dbac091c535e274c8d5f1419967ace72af0623dd
Matt Hackmann <mhackmann13@icloud.com>
2024-02-05 14:05:29 -0600

    add CustomerCheckInView

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 // import CustomerManagerView from 'components/CustomerManagerView';
 import CustomerWaitingListView from 'components/CustomerWaitingListView';
+// import CustomerCheckInView from 'components/CustomerCheckInView';
 
 function App() {
   return (
     <div className="text-eerie_black relative h-screen bg-white">
       <CustomerWaitingListView />
+      {/* <CustomerCheckInView /> */}
       {/* <CustomerManagerView /> */}
     </div>
   );
---
+++ client/src/components/CustomerCheckInView/index.tsx
@@ -0,0 +1,12 @@
+const DUMMY_COMPANY_NAME = 'P&H MGMT LC Troy License Office';
+
+export default function CustomerCheckInView() {
+  return (
+    <div>
+      <div className="flex flex-col items-center">
+        <h1>{DUMMY_COMPANY_NAME}</h1>
+        <h2>Check In</h2>
+      </div>
+    </div>
+  );
+}
