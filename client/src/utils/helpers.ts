e8de8060e82287921a37d9adf2343c6b2d49e709
Matt Hackmann <mhackmann13@icloud.com>
2024-02-11 11:40:27 -0600

    update

--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -171,12 +171,10 @@ const month = [
   'Dec'
 ];
 
-const sameDay = (d1: Date, d2: Date) => {
-  return (
-    d1.getFullYear() === d2.getFullYear() &&
-    d1.getMonth() === d2.getMonth() &&
-    d1.getDate() === d2.getDate()
-  );
+const sameDay = (d1: Date, d2: Date): boolean => {
+  const isSameDay = d1.getDay() === d2.getDay();
+
+  return isSameDay;
 };
 
 const getDateString = (date: Date): string => {
