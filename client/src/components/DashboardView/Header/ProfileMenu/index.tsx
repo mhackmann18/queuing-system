03b46944f3894babcb6a075185ea5079a134969e
Matt Hackmann <mhackmann13@icloud.com>
2024-02-10 22:22:52 -0600

    add ProfileMenu comp to DashboardViewHeader

---
+++ client/src/components/DashboardView/Header/ProfileMenu/index.tsx
@@ -0,0 +1,59 @@
+import { useRef, RefObject, useEffect } from 'react';
+
+interface ProfileMenuProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  buttonRef: RefObject<HTMLButtonElement>;
+  containerStyles?: string;
+}
+
+export default function ProfileMenu({
+  open,
+  setOpen,
+  buttonRef,
+  containerStyles = ''
+}: ProfileMenuProps) {
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  // Close menu when clicking outside of menu
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        open &&
+        menuRef.current &&
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target as Node) &&
+        !menuRef.current.contains(event.target as Node)
+      ) {
+        setOpen(!open);
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [open, setOpen, buttonRef]);
+
+  return (
+    <div
+      ref={menuRef}
+      className={`border-french_gray_2 w-44 rounded-sm border bg-white p-3 text-sm shadow-md
+       ${containerStyles}`}
+    >
+      <button
+        className="bg-french_gray_1-700 hover:bg-french_gray_1 mb-2 block w-full rounded-md
+       px-3 py-2 text-left"
+      >
+        Settings
+      </button>
+      <button
+        className="bg-french_gray_1-700 hover:bg-french_gray_1 block w-full rounded-md px-3
+       py-2 text-left"
+      >
+        Sign Out
+      </button>
+    </div>
+  );
+}
--- client/src/components/DashboardView/Header/index.tsx
+++ client/src/components/DashboardView/Header/index.tsx
@@ -1,27 +1,41 @@
 import CurrentTime from 'components/CurrentTime';
-import React, { useContext } from 'react';
+import { useContext, useState, useRef } from 'react';
 import Breadcrumbs from './Breadcrumbs';
 import { MdAccountCircle } from 'react-icons/md';
 import { UserContext } from 'components/UserContextProvider/context';
+import ProfileMenu from './ProfileMenu';
 
 export default function DashboardHeader() {
   const { username } = useContext(UserContext);
 
+  const [profileMenuOpen, setProfileMenuOpen] = useState(false);
+  const profileButtonRef = useRef<HTMLButtonElement>(null);
+
   return (
     <header className="border-platinum-700 fixed inset-x-0 top-0 z-10 h-16 border-b bg-white shadow-sm">
-      <div className="mx-auto flex h-full max-w-5xl items-center justify-between">
+      <div className="relative mx-auto flex h-full max-w-5xl items-center justify-between">
         <div className="flex items-center">
           <Breadcrumbs />
         </div>
         <div className="flex items-center">
           <CurrentTime styles="mr-6" />
           <button
+            onClick={() => setProfileMenuOpen(!profileMenuOpen)}
+            ref={profileButtonRef}
             type="button"
             className="text-french_gray_2 hover:text-french_gray_2-400 flex items-center"
           >
             <span className="mr-1.5 font-medium">{username}</span>
             <MdAccountCircle className="inline" size={26} />
           </button>
+          {profileMenuOpen && (
+            <ProfileMenu
+              open={profileMenuOpen}
+              setOpen={setProfileMenuOpen}
+              buttonRef={profileButtonRef}
+              containerStyles="absolute right-0 top-14"
+            />
+          )}
         </div>
       </div>
     </header>
