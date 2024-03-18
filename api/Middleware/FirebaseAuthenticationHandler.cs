1e5b804012664934b38badccbd3fd4124adaf93d
Matt Hackmann <mhackmann13@icloud.com>
2024-03-18 10:56:16 -0500

    add better documentation for FirebaseAuthHandler Middleware

--- api/Middleware/FirebaseAuthenticationHandler.cs
+++ api/Middleware/FirebaseAuthenticationHandler.cs
@@ -1,5 +1,3 @@
-
-
 using System.Security.Claims;
 using System.Text.Encodings.Web;
 using FirebaseAdmin.Auth;
@@ -8,6 +6,12 @@ using Microsoft.Extensions.Options;
 
 namespace CustomerApi.Middleware;
 
+/* This class is responsible for authenticating the user using Firebase. 
+   It inherits from the AuthenticationHandler class and overrides the HandleAuthenticateAsync method. 
+   This method is called when the authentication middleware is invoked. 
+   It extracts the token from the Authorization header and verifies it using the Firebase SDK. 
+   If the token is valid, it creates a ClaimsIdentity and a ClaimsPrincipal and returns an AuthenticationTicket with the principal. 
+   If the token is invalid, it returns an AuthenticateResult with a failure message. */
 public class FirebaseAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
 {
     public FirebaseAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder) : base(options, logger, encoder)
@@ -23,6 +27,7 @@ public class FirebaseAuthenticationHandler : AuthenticationHandler<Authenticatio
         if (string.IsNullOrEmpty(authorizationHeader))
             return AuthenticateResult.Fail("Invalid authorization header");
 
+        // Auth header is in the format "Bearer ID_TOKEN", so we need the ID_TOKEN part after the space
         string idToken = authorizationHeader.Split(" ").Last();
 
         try
